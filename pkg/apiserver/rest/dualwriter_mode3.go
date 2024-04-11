23fbcbd2f0a3949407ea2d6f4d9ec36e175f7318
leonorfmartins <leonorfmartins@gmail.com>
2024-04-11 15:08:26 +0200

    [REVIEW] improve comment

--- pkg/apiserver/rest/dualwriter_mode3.go
+++ pkg/apiserver/rest/dualwriter_mode3.go
@@ -43,7 +43,7 @@ func (d *DualWriterMode3) Get(ctx context.Context, name string, options *metav1.
 	return d.Storage.Get(ctx, name, &metav1.GetOptions{})
 }
 
-// Update overrides the generic behavior of the Storage and writes first to US and then to the legacy store.
+// Update overrides the behavior of the generic DualWriter and writes first to Storage and then to LegacyStorage.
 func (d *DualWriterMode3) Update(ctx context.Context, name string, objInfo rest.UpdatedObjectInfo, createValidation rest.ValidateObjectFunc, updateValidation rest.ValidateObjectUpdateFunc, forceAllowCreate bool, options *metav1.UpdateOptions) (runtime.Object, bool, error) {
 	old, err := d.Storage.Get(ctx, name, &metav1.GetOptions{})
 	if err != nil {
